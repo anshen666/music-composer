e1a86bc4094086f5303d2f384acabd806ae4f4fb
hieutpc <47842538+hieutpc@users.noreply.github.com>
2019-02-21 20:12:10 +0700

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -116,7 +116,7 @@ def train(model, network_input, network_output):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=5, batch_size=64, callbacks=callbacks_list)
+    model.fit(network_input, network_output, epochs=3, batch_size=64, callbacks=callbacks_list)
 
 if __name__ == '__main__':
     train_network()
